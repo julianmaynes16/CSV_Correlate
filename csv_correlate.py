fd10e134922378155f7cdb45196eaec00071d040
Julian Maynes <80068880+julianmaynes16@users.noreply.github.com>
2024-07-09 18:14:41 -0700

    Added file opening capabilities

--- csv_correlate.py
+++ csv_correlate.py
@@ -1,2 +1,26 @@
+from glob import glob
+import os
+import pandas as pd
+import csv
+
+force_file = None
+mocap_file = None
+
+def fileStatus():
+    force_file_list = os.listdir(os.path.join(os.getcwd(), 'force'))
+    mocap_file_list = os.listdir(os.path.join(os.getcwd(), 'mocap'))
+    for file in force_file_list: 
+        if file.endswith('.csv'):
+            force_file = os.path.join(os.path.join(os.getcwd(), 'force'), file)
+            print("Found force!")
+
+    for file in mocap_file_list:
+        if(file.endswith(".csv")):
+            mocap_file = os.path.join(os.path.join(os.getcwd(), 'mocap'), file)
+            print("Found mocap!")
 def findStartForce():
-    print("hi")
+
+    print("hi!")
+
+
+fileStatus()

dbca7c18ac6fcdfcf51d0a8b2030a997551079e7
Julian Maynes <80068880+julianmaynes16@users.noreply.github.com>
2024-07-18 18:51:36 -0700

    Fixed unit and type errors

--- csv_correlate.py
+++ csv_correlate.py
@@ -9,6 +9,7 @@ force_file = None
 mocap_file = None
 r = None
 
+frequency_interval = 10
 
 def fileStatus():
     global force_file
@@ -58,7 +59,6 @@ def findStartMocap():
                     print("Done.")
                     break
             mocap_index += 1
-# converts to mm and the origin is in M
 def copyMocapHeights():
     mocap_height = []
     with open(mocap_file, 'r') as csv_file:
@@ -68,29 +68,40 @@ def copyMocapHeights():
         mocap_iterator = 0
         print("Copying mocap data for visualization...")
         for row in reader:
-            if mocap_iterator > 8:
-                mocap_height.append(row[7] * 1000)
+            if (mocap_iterator > 8) and not (mocap_iterator % frequency_interval):
+                #mocap_height.append(mocap_element * 1000)
+                mocap_height.append(float(row[7]))
                 mocap_array_index +=1
             mocap_iterator +=1
+        #mocap_array_index = 0
+        #for item in mocap_height:
+        #    mocap_height[mocap_array_index] = item * 1000
+        #    mocap_array_index += 1
         print("Done.")
         return mocap_height
 # converts to mm and the origin is in M
 def copyForceForces():
     force_height = []
     with open(force_file, 'r') as csv_file:
-        reader = csv.DictReader(csv_file)
+        reader = csv.reader(csv_file)
+        force_force_iterator = 0
         print("Copying force data for visualization...")
         for row in reader:
-            force_height.append(row['Leg 0 Z force'])
+            if (force_force_iterator > 0) and not(force_force_iterator % frequency_interval):
+                force_height.append(abs(float(row[47])) * 1000)
+            force_force_iterator += 1
         print("Done.")
         return force_height
 def copyForceTime():
     force_time = []
     with open(force_file, 'r') as csv_file:
-        reader = csv.DictReader(csv_file)
+        reader = csv.reader(csv_file)
+        force_time_iterator = 0
         print("Copying force time data for visualization...")
         for row in reader:
-            force_time.append(row['Leg 0 Z force'])
+            if (force_time_iterator > 0) and not (force_time_iterator % frequency_interval):
+                force_time.append(float(row[0]))
+            force_time_iterator += 1
         return force_time
 def copyMocapTime():
     mocap_time = []
@@ -100,23 +111,45 @@ def copyMocapTime():
         start_row = 8
         print("Copying mocap time for visualization...")
         for row in reader:
-            if mocap_iterator > start_row:
-                mocap_time.append(row[1])
+            if (mocap_iterator > start_row) and  not(mocap_iterator % frequency_interval):
+                mocap_time.append(float(row[1]))
             mocap_iterator +=1
         print("Done.")
         return mocap_time
+# takes in time at the start force time index, sets start mocap time equal to that 
+# and will subsequent mocap time measurements equal to that time unless the 
+# next mocap time difference is exceeded by the force   
+def shiftMocapTime(mocap_time_array, force_time_array, force_start_index, mocap_start_index):
+    #Sets the starting time of the mocap array to the start time of the force
+    mocap_time_array[mocap_start_index] = force_time_array[force_start_index]
+    mocap_end_iterator = 0
+    for element in mocap_time_array:
+        if mocap_end_iterator > mocap_start_index:
+            #After the start, apply 0 order interpolation (add )
+            # insert 7 time readings that are just same top mocap reading
+        mocap_end_iterator += 1
+
+
 # use mocap's spirit height and knee height and graph the two
 def verifyPlot(force_start, mocap_start):
     mocap_height = copyMocapHeights()
     force_height = copyForceForces()
     force_time = copyForceTime()
     mocap_time = copyMocapTime()
-
-
-    plt.plot(force_height, force_time, label="Knee(mocap)")
+    
+    #print("force_height:")
+    #print(force_height)
+    #print("Force time: ") 
+    #print(force_time)
+    #print("Mocap height: ")
+    #print(mocap_height)
+    #print("Mocap time:")
+    #print(mocap_time)
+    plt.plot(force_height, force_time, label="Knee(force)")
     plt.plot(mocap_height, mocap_time, label ="SPIRIT(mocap)")
     plt.xlabel('Time (s)')
     plt.ylabel('Height()')
+    plt.legend(loc='best')
     plt.title("Mocap-force SPIRIT height to knee height over time")
     print("Showing...")
     plt.show()
